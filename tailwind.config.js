9c5c7f174745500b39a9161133befe7e57229a95
Qusai <qusai.kiwan11@email.com>
2026-05-01 16:00:53 +0300

    Initial AquaTech production site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        glow: "0 0 0 1px rgba(34,211,238,.25), 0 12px 50px rgba(0,0,0,.55)",
+        aqua: "0 18px 60px rgba(96,165,250,.14)"
+      }
+    }
+  },
+  plugins: []
+};
---
+++ test.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+  let res = http.get('https://aqua-tech-t.github.io/aqua-tech-site/');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+  });
+
+  sleep(1);
+}
