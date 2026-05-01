cac4893a96ce33ddc042084105e6e822fc879c22
Qusai <qusai.kiwan11@email.com>
2026-05-01 20:33:13 +0300

    Build CSS and update UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html",
+    "./*.html",
+    "./src/**/*.{js,html}"
+  ],
   theme: {
-    extend: {
-      boxShadow: {
-        glow: "0 0 0 1px rgba(34,211,238,.25), 0 12px 50px rgba(0,0,0,.55)",
-        aqua: "0 18px 60px rgba(96,165,250,.14)"
-      }
-    }
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
