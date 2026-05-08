42053e1a5db38533e6a0334d0e7623c549f44c45
Qusai <qusai.kiwan11@email.com>
2026-05-08 15:19:33 +0300

    improve lighthouse accessibility and performance

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+export const options = {
+  stages: [
+    { duration: '30s', target: 10 },
+    { duration: '1m', target: 25 },
+    { duration: '30s', target: 0 },
+  ],
+};
+
 export default function () {
-  let res = http.get('https://www.aquatechagency.com/');
+  const res = http.get('https://aquatechagency.com/');
 
   check(res, {
     'status is 200': (r) => r.status === 200,
-    'response time < 500ms': (r) => r.timings.duration < 500,
+    'response time < 1s': (r) => r.timings.duration < 1000,
   });
 
   sleep(1);
